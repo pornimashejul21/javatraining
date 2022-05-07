5655dbb1b98013310d1c256d5e318d2a08039c8c
pornimashejul21 <pornimashejul2001@gmail.com>
2022-05-07 16:21:07 +0530

    initial commit

---
+++ Hash and Equals/Hash1.java
@@ -0,0 +1,65 @@
+import java.util.*;
+class Student{
+int id;
+String name;
+
+
+Student(int a, String b){
+id=a;
+name=b;
+
+}
+//toString() is used to make value in readable format.
+
+public String toString(){
+
+return "(" +id+ ", " +name+")";
+}
+@Override
+public int hashCode(){
+int code;
+code = name.hashCode()%id;
+return code;
+
+
+}
+public boolean equals(Object obj){
+boolean f = false;
+
+Student st = (Student)obj;
+if(id==st.id && name.equals(st.name)){
+f= true;
+
+
+}
+return f;
+}
+
+
+}
+
+public class Hash1{
+public static void main(String args[]){
+HashSet<Student>hs;
+hs= new HashSet<>();
+
+Student st1 = new Student(1,"A");
+Student st2 = new Student(1,"A");
+hs.add(st1);
+hs.add(st2);
+
+System.out.println(hs);
+System.out.println(st1.hashCode());
+System.out.println(st2.hashCode());
+
+
+}
+
+
+
+
+
+
+
+
+}
