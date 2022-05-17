54bef0ad1adc0fcee11bf1e157cc205df725f32b
pornimashejul21 <pornimashejul2001@gmail.com>
2022-05-17 22:37:01 +0530

    initial commit

---
+++ Day 2/Math1.java
@@ -0,0 +1,14 @@
+//rad = degrees * Math.PI/180
+
+class Math1 {
+    public static void main(String args[]) {
+        var degrees = 30;
+        double rad;
+
+        rad = degrees * Math.PI / 180;
+
+        System.out.println(rad);
+
+    }
+
+}
---
+++ Day 2/Quadradic.java
@@ -0,0 +1,8 @@
+class Quadradic {
+    public static void main(String[] args) {
+        double value = 4;
+        double quadvalue = (3 * (4 ^ 2) + (-8 * 4) + (4));
+        System.out.println("Quadradic: " + value);
+        System.out.println("Quadradic: " + quadvalue);
+    }
+}
