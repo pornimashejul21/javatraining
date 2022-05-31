a2d0419111a0a217adb0ac74007e50b1b076fb49
pornimashejul21 <pornimashejul2001@gmail.com>
2022-05-31 12:56:01 +0530

    initial commit

---
+++ ShoppingCartApp/DemoCart/src/main/java/com/example/DemoCart/DemoCartApplication.java
@@ -0,0 +1,13 @@
+package com.example.DemoCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCartApplication.class, args);
+	}
+
+}
